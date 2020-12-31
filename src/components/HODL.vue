df8eeb3faf20c3e07644fb28056e439b72f23574
ben <ben@elselab.com>
2020-12-30 18:54:02 -0800

    Update HODL.vue
    updated and dist folder.

--- src/components/HODL.vue
+++ src/components/HODL.vue
@@ -8,10 +8,12 @@
     </div>
 
     <p>
-      Elselab is BRB. Perhaps you're looking for 
-      <a href="https://www.squacks.co/" target="_blank" rel="noopener">Ben Mansell</a>
-      or 
-      <a href="https://patater.com/" target="_blank" rel="noopener">Jaeden Amero</a>.
+      Elselab is BRB. 
+      <br />
+      Perhaps you're looking for 
+      <a href="https://patater.com/" target="_blank" rel="noopener">Jaeden Amero</a>
+      or
+      <a href="https://www.squacks.co/" target="_blank" rel="noopener">Ben Mansell</a>.
     </p>
   </div>
 </div>
@@ -42,7 +44,7 @@ export default {
   place-items: center;
   &-orb {
     margin-top: 4rem;
-    width: 400px;
+    width: 240px;
   }
   .st0, .st1 {
     fill: none;
@@ -50,15 +52,20 @@ export default {
     stroke-miterlimit:10;
   }
   p {
-
     max-width: 520px;
     color: white;
     text-align: center;
-    font-size: var(--fs-2);
+    // font-size: var(--fs-1);
     // place-items: end;
+    widows: 2;
   }
   a {
     text-decoration: underline;
+  }
+    @media only screen and (min-width: 768px) {
+    .HODL-orb {
+      width: 400px;
+    }
   }
 }
 
